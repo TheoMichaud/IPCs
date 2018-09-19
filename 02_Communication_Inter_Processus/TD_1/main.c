a111f464c3733606e3270a69e03386267cb922b3
TheoMichaud <theo15@live.fr>
2018-09-19 09:20:52 +0200

    19/09/2018 09:20

---
+++ 02_Communication_Inter_Processus/TD_1/main.c
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: tmichaud
+ *
+ * Created on 19 septembre 2018, 09:17
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+
+
+int main ( int argc, char *argv[] )
+{
+	
+	fork();
+	fork();
+	fork();
+	printf("* ");
+	
+
+	return EXIT_SUCCESS;
+}
+
+
