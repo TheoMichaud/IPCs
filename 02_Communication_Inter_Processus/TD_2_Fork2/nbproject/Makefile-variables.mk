63cffc16e9e2ca7ba7125394289b564cff4379b8
TheoMichaud <theo15@live.fr>
2018-09-21 11:45:41 +0200

    21/09/2018 11:45

--- 02_Communication_Inter_Processus/TD_2_Fork2/nbproject/Makefile-variables.mk
+++ 02_Communication_Inter_Processus/TD_2_Fork2/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-Linux
 CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
-CND_ARTIFACT_NAME_Debug=td_2
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/td_2
+CND_ARTIFACT_NAME_Debug=td_2_fork2
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/td_2_fork2
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
-CND_PACKAGE_NAME_Debug=td2.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/td2.tar
+CND_PACKAGE_NAME_Debug=td2fork2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/td2fork2.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
-CND_ARTIFACT_NAME_Release=td_2
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/td_2
+CND_ARTIFACT_NAME_Release=td_2_fork2
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/td_2_fork2
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
-CND_PACKAGE_NAME_Release=td2.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/td2.tar
+CND_PACKAGE_NAME_Release=td2fork2.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/td2fork2.tar
 #
 # include compiler specific variables
 #
--- 02_Communication_Inter_Processus/TD_2_Fork2/nbproject/Package-Debug.bash
+++ 02_Communication_Inter_Processus/TD_2_Fork2/nbproject/Package-Debug.bash
@@ -7,15 +7,15 @@
 # Macros
 TOP=`pwd`
 CND_PLATFORM=GNU-Linux
-CND_CONF=Release
+CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
 CND_DLIB_EXT=so
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/td_2
-OUTPUT_BASENAME=td_2
-PACKAGE_TOP_DIR=td2/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/td_2_fork2
+OUTPUT_BASENAME=td_2_fork2
+PACKAGE_TOP_DIR=td2fork2/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/td2/bin"
+makeDirectory "${NBTMPDIR}/td2fork2/bin"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/td2.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/td2fork2.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/td2.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/td2fork2.tar *
 checkReturnCode
 
 # Cleanup
